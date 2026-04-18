c1b3e17b0c8f23e66a50e0699dceba1541c3767b
pavankumar7022 <pavankumar.sc2022@gmail.com>
2026-04-18 12:56:42 +0530

    fixed error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const PORT = process.env.PORT||3000;
+
 const app = express();
 app.use(express.json());
 
